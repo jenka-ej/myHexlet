72c4e78fba04cba312a38fe1c0015cc008765ed8
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-11 16:07:00 +0300

    Create removeFirstLevel.js
    Реализуйте и экспортируйте по умолчанию функцию, которая принимает на вход дерево, и возвращает новое, элементами которого являются дети вложенных узлов.

---
+++ Module 3 - Professional JS/JS: Trees/removeFirstLevel.js
@@ -0,0 +1,25 @@
+const removeFirstLevel = (mass) => {
+  const result = mass.filter(Array.isArray);
+
+  return result.flat();
+};
+
+export default removeFirstLevel;
+
+/* test */
+
+import removeFirstLevel from '../removeFirstLevel.js';
+
+test('removeFirstLevel', () => {
+  const tree1 = [];
+  const result1 = [];
+  expect(removeFirstLevel(tree1)).toEqual(result1);
+
+  const tree2 = [1, 100, 3];
+  const result2 = [];
+  expect(removeFirstLevel(tree2)).toEqual(result2);
+
+  const tree3 = [[1, [3, 2]], 2, { a: 1 }, [3, 5], 2];
+  const result3 = [1, [3, 2], 3, 5];
+  expect(removeFirstLevel(tree3)).toEqual(result3);
+});
