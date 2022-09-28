00093ce2ba993207350d80b77e51078fe4616d0a
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-28 10:55:58 +0300

    Create 10: objects.js
    Реализуйте и экспортируйте по умолчанию функцию, которая выполняет глубокое копирование объектов.

---
+++ Module 3 - Professional JS/JS: Objects/10: objects.js
@@ -0,0 +1,39 @@
+import isObject from 'lodash/isObject.js';
+
+const cloneDeep = (obj) => {
+  const getChildren = (obj1) => Object.keys(obj1);
+  const massiveKeys = getChildren(obj);
+  const cb = (acc, key) => {
+    const value = obj[key];
+    if (isObject(value)) {
+      acc[key] = cloneDeep(value);
+      return acc;
+    }
+    acc[key] = value;
+    return acc;
+  };
+  return massiveKeys.reduce(cb, {});
+};
+export default cloneDeep;
+
+/* __tests__ */
+
+import cloneDeep from '../objects.js';
+
+const data = {
+  key: 'value',
+  key2: {
+    key: 'innerValue',
+    innerKey: {
+      anotherKey: 'anotherValue',
+    },
+  },
+};
+
+it('deepClone', () => {
+  const clonedData = cloneDeep(data);
+  expect(clonedData).toEqual(data);
+  expect(clonedData).not.toBe(data);
+  expect(clonedData.key2).not.toBe(data.key2);
+  expect(clonedData.key2.innerKey).not.toBe(data.key2.innerKey);
+});
