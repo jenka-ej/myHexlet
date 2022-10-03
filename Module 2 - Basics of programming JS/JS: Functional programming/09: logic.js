8b8f3b05d93ec68973083413562956cbf35f0be5
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-10-03 23:54:03 +0300

    Create 09: logic.js
    Удивительно, но факт — условные конструкции можно реализовать самостоятельно с использованием одних лишь каррированных функций.

---
+++ Module 2 - Basics of programming JS/JS: Functional programming/09: logic.js
@@ -0,0 +1,14 @@
+export const If = (condition) => (a) => (b) => condition(a)(b);
+export const True = (a) => () => a;
+export const False = () => (b) => b;
+
+/* __tests__ */
+
+import { If, True, False } from '../logic.js';
+
+test('Boolean', () => {
+  expect(If(True)('fo')('bar')).toBe('fo');
+  expect(If(False)('foo')('ba')).toBe('ba');
+  expect(True()()).toBeUndefined();
+  expect(False()()).toBeUndefined();
+});
