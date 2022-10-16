f04549be22a6fc6de2d696a087d038d0eb7b1b7c
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-10-17 00:18:26 +0300

    Create 08: partialApply.js

---
+++ Module 4 - Structures and Interpretations of Computer Programs JS/JS: Functional programming/08: partialApply.js
@@ -0,0 +1,18 @@
+// Реализуйте и экспортируйте по умолчанию функцию, которая умеет частично применять один (второй) аргумент у переданной функции c двумя аргументами.
+
+const partialApply = (f, fix) => (a) => f(a, fix);
+export default partialApply;
+
+/* __tests__ */
+
+import partialApply from '../partialApply';
+
+test('partialApply', () => {
+  const f1 = partialApply((a, b) => a ** b, 2);
+  expect(f1(1)).toBe(1);
+  expect(f1(10)).toBe(100);
+
+  const f2 = partialApply((a, b) => a * b, 5);
+  expect(f2(2)).toBe(10);
+  expect(f2(5)).toBe(25);
+});
