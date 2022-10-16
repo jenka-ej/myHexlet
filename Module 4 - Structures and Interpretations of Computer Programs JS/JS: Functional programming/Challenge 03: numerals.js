a1b9fec12ef6036f5a675cdeab3ed15860e3cda6
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-10-17 00:14:25 +0300

    Create Challenge 03: numerals.js

---
+++ Module 4 - Structures and Interpretations of Computer Programs JS/JS: Functional programming/Challenge 03: numerals.js
@@ -0,0 +1,18 @@
+// Реализуйте алгоритм числа Zero() и операции Succ() (увеличение числа на единицу) в соответствии с арифметикой Чёрча. Не забудьте про экспорт.
+
+export const Zero = () => (z) => z;
+export const Succ = (n) => (s) => (z) => s(n(s)(z));
+
+/* __tests__ */
+
+import { Zero, Succ } from '../numerals.js';
+
+it('numerals', () => {
+  const two = Succ(Succ(Zero));
+  expect(two((x) => x + 1)(0)).toBe(2);
+
+  const four = Succ(Succ(two));
+  expect(four((x) => x + 1)(0)).toBe(4);
+
+  expect(four(two((x) => x + 1))(0)).toBe(8);
+});
