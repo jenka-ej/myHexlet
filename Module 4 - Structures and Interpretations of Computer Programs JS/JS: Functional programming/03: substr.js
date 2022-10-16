7f9f3c8b37d35748e18e729d196a11aa2ace4a92
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-10-17 00:25:48 +0300

    Create 03: substr.js

---
+++ Module 4 - Structures and Interpretations of Computer Programs JS/JS: Functional programming/03: substr.js
@@ -0,0 +1,52 @@
+// Реализуйте и экспортируйте по умолчанию функцию, которая возвращает подстроку из переданной строки. 
+// Подстрокой называется любая выбранная часть строки. Например для строки hello можно выделить следующие подстроки: ello, llo, o, el и множество других. 
+// Функция принимает на вход три параметра:
+// 1) Строка
+// 2) Начальный индекс. Значение по умолчанию: 0
+// 3) Длина подстроки (количество символов). Значение по умолчанию: длина всей строки
+
+const substr = (str, firstIndex = 0, lengthSubstring = str.length) => {
+  let newFirstIndex = firstIndex;
+  let newLengthSubstring = lengthSubstring;
+  if (newFirstIndex < 0) {
+    newFirstIndex = 0;
+  }
+  if (newLengthSubstring === 0 || newFirstIndex >= str.length) {
+    return '';
+  }
+  if (newLengthSubstring < 0) {
+    newLengthSubstring = 1;
+  }
+  let result = '';
+  if (newFirstIndex + newLengthSubstring > str.length) {
+    for (let i = newFirstIndex; i < str.length; i += 1) {
+      result += str[i];
+    }
+    return result;
+  }
+  for (let i = newFirstIndex; i < newFirstIndex + newLengthSubstring; i += 1) {
+    result += str[i];
+  }
+  return result;
+};
+export default substr;
+
+/* __tests__ */
+
+import substr from '../substr';
+
+test('substr', () => {
+  expect(substr('')).toEqual('');
+  expect(substr('abba', 4, -10)).toEqual('');
+  expect(substr('abba')).toEqual('abba');
+  expect(substr('abba', 1, 0)).toEqual('');
+  expect(substr('abba', 0, 1)).toEqual('a');
+  expect(substr('abba', 1, 2)).toEqual('bb');
+  expect(substr('abba', -10, 2)).toEqual('ab');
+  expect(substr('abba', -1, 100)).toEqual('abba');
+  expect(substr('abba', -1, -1)).toEqual('a');
+  expect(substr('abba', 1, -10)).toEqual('b');
+  expect(substr('abba', 1, 10)).toEqual('bba');
+  expect(substr('abba', 0, 4)).toEqual('abba');
+  expect(substr('abba', 0, 5)).toEqual('abba');
+});
