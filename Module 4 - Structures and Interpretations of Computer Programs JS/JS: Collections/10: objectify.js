57124d066b5e42701b9b5829011f8ef0bfbb9ec2
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-10-17 00:01:11 +0300

    Create 10: objectify.js

---
+++ Module 4 - Structures and Interpretations of Computer Programs JS/JS: Collections/10: objectify.js
@@ -0,0 +1,91 @@
+// Реализуйте и экспортируйте по умолчанию функцию, которая работает следующим образом:
+// 1) Принимает на вход список (в виде обычного js массива с объектами внутри) и функцию-селектор, выбирающую из каждого объекта определенное значение.
+// 2) Возвращает объект, в котором ключ - это результат применения функции селектора к каждому объекту в массиве, а значение - это сам объект.
+// Обратите внимание на то, что эта функция высшего порядка является универсальной и работает с любыми наборами данных.
+
+const objectify = (coll, fn) => {
+  const compare = (a, b) => {
+    const fn1 = fn(a);
+    const fn2 = fn(b);
+    if (fn1 > fn2) {
+      return 1;
+    }
+    if (fn1 < fn2) {
+      return -1;
+    }
+    return 0;
+  };
+  const sortColl = coll.sort(compare);
+  const cb = (acc, obj) => {
+    acc[fn(obj)] = obj;
+    return acc;
+  };
+  return sortColl.reduce(cb, {});
+};
+export default objectify;
+
+/* __tests__ */
+
+import objectify from '../objectify';
+
+const cars = [
+  {
+    id: 1, brand: 'bmw', model: 'm5', year: 2014,
+  },
+  {
+    id: 8, brand: 'bmw', model: 'm4', year: 2013,
+  },
+  {
+    id: 100, brand: 'kia', model: 'sorento', year: 2014,
+  },
+  {
+    id: 93, brand: 'kia', model: 'rio', year: 2010,
+  },
+  {
+    id: 70, brand: 'kia', model: 'sportage', year: 2012,
+  },
+];
+
+const cars2 = [
+  { brand: 'bmw', model: 'm3', year: 2013 },
+  { brand: 'opel', model: 'astra', year: 2014 },
+  { brand: 'hyundai', model: 'accent', year: 2014 },
+  { brand: 'kia', model: 'rio', year: 2013 },
+  { brand: 'kia', model: 'sportage', year: 2015 },
+];
+
+test('objectify', () => {
+  const actual1 = objectify(cars, (car) => car.id);
+
+  const expected1 = {
+    1: {
+      brand: 'bmw', id: 1, model: 'm5', year: 2014,
+    },
+    70: {
+      brand: 'kia', id: 70, model: 'sportage', year: 2012,
+    },
+    100: {
+      id: 100, brand: 'kia', model: 'sorento', year: 2014,
+    },
+    8: {
+      brand: 'bmw', id: 8, model: 'm4', year: 2013,
+    },
+    93: {
+      brand: 'kia', id: 93, model: 'rio', year: 2010,
+    },
+  };
+
+  expect(actual1).toEqual(expected1);
+
+  const actual2 = objectify(cars2, (car) => car.model);
+
+  const expected2 = {
+    accent: { brand: 'hyundai', model: 'accent', year: 2014 },
+    astra: { brand: 'opel', model: 'astra', year: 2014 },
+    m3: { brand: 'bmw', model: 'm3', year: 2013 },
+    rio: { brand: 'kia', model: 'rio', year: 2013 },
+    sportage: { brand: 'kia', model: 'sportage', year: 2015 },
+  };
+
+  expect(actual2).toEqual(expected2);
+});
