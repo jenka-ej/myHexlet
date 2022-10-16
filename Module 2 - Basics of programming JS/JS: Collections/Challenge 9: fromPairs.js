e305f2fa2b2da45352d3b4b88d05a4093ed29602
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-10-16 22:31:16 +0300

    Create Challenge 9: fromPairs.js
    Реализуйте и экспортируйте функцию по умолчанию, которая принимает на вход массив, состоящий из массивов-пар и возвращает объект, полученный из этих пар.

---
+++ Module 2 - Basics of programming JS/JS: Collections/Challenge 9: fromPairs.js
@@ -0,0 +1,22 @@
+const fromPairs = (coll) => coll
+  .reduce((acc, pair) => {
+    const [key, value] = pair;
+    acc[key] = value;
+    return acc;
+  }, {});
+export default fromPairs;
+
+/* __tests__ */
+
+import fromPairs from '../fromPairs.js';
+
+test('fromPairs', () => {
+  const result1 = fromPairs([['fred', 30], ['barney', 40]]);
+  expect(result1).toEqual({ fred: 30, barney: 40 });
+
+  const result2 = fromPairs([['cat', 5], ['dog', 6], ['bird', 10]]);
+  expect(result2).toEqual({ cat: 5, dog: 6, bird: 10 });
+
+  const result3 = fromPairs([['cat', 5], ['dog', 6], ['cat', 11]]);
+  expect(result3).toEqual({ cat: 11, dog: 6 });
+});
