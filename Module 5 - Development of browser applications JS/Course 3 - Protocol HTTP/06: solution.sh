e84f17ea8b098a5b675acc445ec833258038ad24
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-12-15 11:13:43 +0300

    Create 06: solution.sh

---
+++ Module 5 - Development of browser applications JS/Course 3 - Protocol HTTP/06: solution.sh
@@ -0,0 +1,46 @@
+# Используя telnet выполните запрос к hexlet.local (расположен на localhost) на порт 8080. Параметры запроса: метод GET, страница /stream, протокол HTTP 1.1.
+
+GET /stream HTTP/1.1
+Host: hexlet.local
+Connection: close
+
+# __tests__
+
+import fs from 'fs';
+import url from 'url';
+import httpHeaders from 'http-headers';
+import build from '../server/index.js';
+
+test('should work', async () => {
+  const app = build();
+
+  const data = fs.readFileSync('solution', 'utf-8');
+  const requestObj = httpHeaders(
+    data
+      .split('\n')
+      .map((el) => el.trim())
+      .join('\r\n'),
+  );
+
+  const { headers: { host } } = requestObj;
+  expect(host).toEqual('hexlet.local');
+
+  expect(requestObj.version).toEqual({ major: 1, minor: 1 });
+
+  const parts = {
+    port: 8080,
+    protocol: 'http',
+    hostname: 'localhost',
+    pathname: requestObj.url,
+  };
+  const requestUrl = url.format(parts);
+
+  const options = {
+    headers: requestObj.headers,
+    method: requestObj.method,
+    url: requestUrl,
+  };
+
+  const { statusCode } = await app.inject(options);
+  expect(statusCode).toEqual(200);
+});
