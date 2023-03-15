2adf9787b16d0c7a8f1274a1b4be49b627e2ebae
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-03-15 11:34:43 +0300

    Create 09: getFilesCount.test.js

---
+++ Module 5 - Development of browser applications JS/Course 8 - JS: Advanced testing/09: getFilesCount.test.js
@@ -0,0 +1,55 @@
+// Протестируйте функцию getFilesCount(path, log), которая считает количество всех файлов в указанной директории и всех поддиректориях. 
+// В отличие от предыдущей версии задания, здесь нас интересует только то, что эта функция выполняет логирование. 
+// Мы хотим убедиться, что она один раз отправляет сообщение c текстом 'Go!' в лог. Для этого придётся воспользоваться моком.
+
+/* functions.js */
+
+import klawSync from 'klaw-sync';
+import winston from 'winston';
+
+const defaultLogger = winston.createLogger();
+defaultLogger.on('finish', () => {
+  throw new Error('Cannot send data to log!');
+});
+
+const functions = {
+  right1: (filepath, log = () => defaultLogger.end()) => {
+    log('Go!');
+    return klawSync(filepath, { nodir: true }).length;
+  },
+  wrong1: (filepath, log = () => defaultLogger.end()) => {
+    log('Go!');
+    log('Go!');
+    return klawSync(filepath, { nodir: true }).length;
+  },
+  wrong2: (filepath, log = () => defaultLogger.end()) => {
+    log('Boom!');
+    return klawSync(filepath, { nodir: true }).length;
+  },
+};
+
+export default () => {
+  const name = process.env.FUNCTION_VERSION || 'right1';
+  return functions[name];
+};
+
+/* __tests__ */
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { jest } from '@jest/globals';
+import getFunction from '../functions.js';
+
+const getFilesCount = getFunction();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
+
+test('log', () => {
+  const callback = jest.fn();
+
+  getFilesCount(getFixturePath('nested'), callback);
+  expect(callback.mock.calls).toHaveLength(1);
+  expect(callback.mock.calls[0][0]).toBe('Go!');
+});
