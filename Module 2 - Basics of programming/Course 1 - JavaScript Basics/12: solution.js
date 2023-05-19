7bfa65984aaeedcbdabb0bc0446c82ba4609f666
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-05-19 16:04:52 +0300

    Create 12: solution.js

---
+++ Module 2 - Basics of programming/Course 1 - JavaScript Basics/12: solution.js
@@ -0,0 +1,25 @@
+// Выведите на экран число -0.304.
+
+console.log(-0.304);
+
+/* __tests__ */
+
+import {
+  jest, test, beforeEach, expect,
+} from '@jest/globals';
+
+const getPathToSolution = () => `${process.cwd()}/solution.js`;
+
+let spy;
+
+beforeEach(() => {
+  spy = jest.spyOn(console, 'log');
+});
+
+test('solution output', async () => {
+  const expected = '-0.304';
+  await import(getPathToSolution());
+
+  const output = spy.mock.calls.map((args) => args.join(' ')).join('\n');
+  expect(output).toEqual(expected);
+});
