e354546ab1f8193f436294332301344c2090b99a
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-05-19 15:50:15 +0300

    Create 02: solution.js

---
+++ Module 2 - Basics of programming/Course 1 - JavaScript Basics/02: solution.js
@@ -0,0 +1,26 @@
+// Наберите в редакторе код из примера ниже символ в символ и нажмите «Проверить».
+// console.log('Hello, World!')
+
+console.log('Hello, World!');
+
+/* __tests__ */
+
+import {
+  jest, test, beforeEach, expect,
+} from '@jest/globals';
+
+const getPathToSolution = () => `${process.cwd()}/solution.js`;
+
+let spy;
+
+beforeEach(() => {
+  spy = jest.spyOn(console, 'log');
+});
+
+test('solution output', async () => {
+  const expected = 'Hello, World!';
+  await import(getPathToSolution());
+
+  const output = spy.mock.calls.map((args) => args.join(' ')).join('\n');
+  expect(output).toEqual(expected);
+});
