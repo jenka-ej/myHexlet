abf424b14083650947fc0a6e9a022bddee35c9f2
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-06-06 18:13:52 +0300

    Create 10: Normal.js

---
+++ Module 6 - React application development/Course 1 - JS: Polymorphism/10: Normal.js
@@ -0,0 +1,84 @@
+// Реализуйте стратегию, которая пытается заполнить поля, пробегаясь построчно слева направо и снизу вверх (начиная с левого нижнего угла). 
+// Как только она встречает свободное поле, то вставляет туда значение.
+
+class Normal {
+  constructor() {
+    this.moves = [6, 7, 8, 3, 4, 5, 0, 1, 2];
+  }
+
+  move() {
+    return this.moves;
+  }
+}
+
+export default Normal;
+
+/* __tests__ */
+
+import TicTacToe from '../TicTacToe.js';
+
+describe('TicTacToe', () => {
+  it('test easy game 1', () => {
+    const game = new TicTacToe();
+    game.go(1, 1);
+    game.go();
+    game.go(0, 1);
+    game.go();
+    const isWinner = game.go(2, 1);
+    expect(isWinner).toBeTruthy();
+  });
+
+  it('test easy game 2', () => {
+    const game = new TicTacToe();
+    game.go(1, 1);
+    game.go();
+    game.go(1, 2);
+    expect(game.go()).toBeFalsy();
+    expect(game.go(2, 2)).toBeFalsy();
+    const isWinner = game.go();
+    expect(isWinner).toBeTruthy();
+  });
+
+  it('test easy game 3', () => {
+    const game = new TicTacToe();
+    game.go(0, 0);
+    game.go();
+    expect(game.go(1, 1)).toBeFalsy();
+    expect(game.go()).toBeFalsy();
+    const isWinner = game.go(2, 2);
+    expect(isWinner).toBeTruthy();
+  });
+
+  it('test normal game 1', () => {
+    const game = new TicTacToe('normal');
+    game.go(0, 2);
+    game.go();
+    game.go(0, 1);
+    expect(game.go()).toBeFalsy();
+    expect(game.go(1, 2)).toBeFalsy();
+    const isWinner = game.go();
+    expect(isWinner).toBeTruthy();
+  });
+
+  it('test normal game 2', () => {
+    const game = new TicTacToe('normal');
+    game.go();
+    game.go(2, 1);
+    game.go();
+    game.go(1, 0);
+    expect(game.go()).toBeFalsy();
+    expect(game.go(1, 2)).toBeFalsy();
+    const isWinner = game.go();
+    expect(isWinner).toBeTruthy();
+  });
+
+  it('test normal game 3', () => {
+    const game = new TicTacToe('normal');
+    game.go(2, 2);
+    game.go();
+    expect(game.go(1, 1)).toBeFalsy();
+    expect(game.go()).toBeFalsy();
+    const isWinner = game.go(0, 0);
+    expect(isWinner).toBeTruthy();
+  });
+});
