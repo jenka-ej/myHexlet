2967482aca2b514a794e2372f8aefb357dc67ede
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-06-16 11:47:13 +0300

    Update 02: HTMLElement.js

--- Module 6 - React application development/Course 2 - JS: Diving into classes/02: HTMLElement.js
+++ Module 6 - React application development/Course 2 - JS: Diving into classes/02: HTMLElement.js
@@ -13,6 +13,18 @@ export default class HTMLElement {
   }
 }
 
+/* HTMLHrElement.js */
+// Реализуйте класс HTMLHrElement (наследуется от HTMLElement), который отвечает за представление тега <hr>.
+// Внутри класса определите функцию toString(), которая возвращает текстовое представление тега.
+
+import HTMLElement from './HTMLElement.js';
+
+export default class HTMLHrElement extends HTMLElement {
+  toString() {
+    return `<hr${this.stringifyAttributes()}>`;
+  }
+}
+
 /* __tests__ */
 
 import HTMLHrElement from '../HTMLHrElement.js';
