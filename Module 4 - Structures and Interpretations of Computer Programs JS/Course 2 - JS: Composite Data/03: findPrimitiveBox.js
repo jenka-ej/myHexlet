50d6518578a60738ae056d15058f48c85255b362
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-06-16 12:10:56 +0300

    Update 03: findPrimitiveBox.js

--- Module 4 - Structures and Interpretations of Computer Programs JS/Course 2 - JS: Composite Data/03: findPrimitiveBox.js
+++ Module 4 - Structures and Interpretations of Computer Programs JS/Course 2 - JS: Composite Data/03: findPrimitiveBox.js
@@ -22,7 +22,7 @@ const findPrimitiveBox = (pair) => {
 };
 export default findPrimitiveBox;
 
-/* __tests__ */
+/* __tests__/findPrimitiveBox.test.js */
 
 import { cons, toString } from '@hexlet/pairs';
 import findPrimitiveBox from '../findPrimitiveBox.js';
@@ -45,4 +45,44 @@ test('findPrimitiveBox', () => {
     cons('one', true),
   );
   expect(toString(findPrimitiveBox(pair3))).toBe(toString(cons('one', true)));
+});
+
+/* reversePair.js */
+// Реализуйте функцию reversePair(), которая принимает на вход пару и возвращает другую, в которой значения переставлены местами.
+
+import { cons, car, cdr, toString } from '@hexlet/pairs'; // eslint-disable-line
+
+const reversePair = (pair) => cons(cdr(pair), car(pair));
+export default reversePair;
+
+/* __tests__/reversePair.test.js */
+
+import { cons, toString } from '@hexlet/pairs';
+import reversePair from '../reversePair.js';
+
+test('reversePair', () => {
+  expect(toString(reversePair(cons(4, 3)))).toBe(toString(cons(3, 4)));
+  expect(toString(reversePair(cons(-10, 1)))).toBe(toString(cons(1, -10)));
+});
+
+/* sumOfPairs.js */
+// Реализуйте функцию sumOfPairs(), которая принимает на вход две пары и возвращает новую пару, в элементах которой находятся суммы элементов из исходных пар.
+
+import { cons, car, cdr, toString } from '@hexlet/pairs'; // eslint-disable-line
+
+const sumOfPairs = (pair1, pair2) => {
+  const sumCar = car(pair1) + car(pair2);
+  const sumCdr = cdr(pair1) + cdr(pair2);
+  return cons(sumCar, sumCdr);
+};
+export default sumOfPairs;
+
+/* __tests__/sumOfPairs.test.js */
+
+import { cons, toString } from '@hexlet/pairs';
+import sumOfPairs from '../sumOfPairs.js';
+
+test('sumOfPairs', () => {
+  expect(toString(sumOfPairs(cons(1, 8), cons(8, 3)))).toBe(toString(cons(9, 11)));
+  expect(toString(sumOfPairs(cons(10, -1), cons(93, 100)))).toBe(toString(cons(103, 99)));
 });
