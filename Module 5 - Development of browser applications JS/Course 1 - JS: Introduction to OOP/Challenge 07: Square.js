1b8c95a9f404f63bda577621a08476a1a075d3c5
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-06-16 12:21:03 +0300

    Update Challenge 07: Square.js

--- Module 5 - Development of browser applications JS/Course 1 - JS: Introduction to OOP/Challenge 07: Square.js
+++ Module 5 - Development of browser applications JS/Course 1 - JS: Introduction to OOP/Challenge 07: Square.js
@@ -11,7 +11,7 @@ export default class Square {
   }
 }
 
-/* __tests__ */
+/* __tests__/Square.test.js */
 
 import Square from '../Square.js';
 
@@ -19,4 +19,44 @@ test('GetSide', () => {
   const square = new Square(4);
   const actual = square.getSide();
   expect(actual).toBe(4);
+});
+
+/* SquaresGenerator.js */
+// Реализуйте класс SquaresGenerator со статическим методом generate(), принимающим два параметра: сторону и количество экземпляров квадрата (по умолчанию 5 штук),
+// которые нужно создать. 
+// Функция должна вернуть массив из квадратов. Экспортируйте класс по умолчанию.
+
+import Square from './Square.js';
+
+export default class SquaresGenerator {
+  static generate(side, quantity = 5) {
+    const result = [];
+    for (let i = 0; i < quantity; i += 1) {
+      result.push(new Square(side));
+    }
+    return result;
+  }
+}
+
+/* __tests__/SquaresGenerator.test.js */
+
+import SquaresGenerator from '../SquaresGenerator.js';
+import Square from '../Square.js';
+
+test('SquaresGenerator #1', () => {
+  const squares = SquaresGenerator.generate(1, 1);
+  const expected = [new Square(1)];
+  expect(squares).toEqual(expected);
+});
+
+test('SquaresGenerator #2', () => {
+  const squares = SquaresGenerator.generate(3, 2);
+  const expected = [new Square(3), new Square(3)];
+  expect(squares).toEqual(expected);
+  expect(squares[0]).not.toBe(squares[1]);
+});
+
+test('SquaresGenerator #3 default count', () => {
+  const squares = SquaresGenerator.generate(2);
+  expect(squares).toHaveLength(5);
 });
