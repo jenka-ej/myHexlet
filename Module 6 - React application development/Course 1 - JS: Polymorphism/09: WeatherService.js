6c34f0a6faaf223a8fbea90cb4a8a927e4fe275f
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-06-16 11:56:58 +0300

    Update 09: WeatherService.js

--- Module 6 - React application development/Course 1 - JS: Polymorphism/09: WeatherService.js
+++ Module 6 - React application development/Course 1 - JS: Polymorphism/09: WeatherService.js
@@ -20,6 +20,20 @@ export default class WeatherService {
   }
 }
 
+/* weather.js */
+// Реализуйте код, вызывающий сервис и печатающий на экран ожидаемую строку. Для извлечения города из аргументов командной строки, 
+// воспользуйтесь свойством argv глобального объекта process. 
+// Первый аргумент (передаваемое имя города) находится под индексом 2. Посмотреть описание и пример можно в документации.
+
+import axios from 'axios';
+import WeatherService from '../WeatherService.js';
+
+const city = process.argv[2];
+const weather = new WeatherService(axios, city);
+const response = await weather.get();
+const { temperature } = JSON.parse(response.data);
+console.log(`Temperature in ${city}: ${temperature}C`);
+
 /* __tests__ */
 
 import { execSync } from 'child_process';
