782b6a7530e534bf8184fdedf1101d81d6ffd9bd
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-09-25 16:02:21 +0300

    Create Challenge 03: chunk.js

---
+++ Module 2 - Basics of programming/Course 2 - JS: Arrays/Challenge 03: chunk.js
@@ -0,0 +1,33 @@
+// Реализуйте и экспортируйте функцию по умолчанию, которая принимает на вход массив и число, которое задает размер чанка (куска).
+// Функция должна вернуть массив, состоящий из чанков указанной размерности.
+
+export default (mass, num) => {
+  const result = [];
+  for (let i = 0; i < mass.length; i += num) {
+    result.push(mass.slice(i, i + num));
+  }
+  return result;
+};
+
+/* __tests__ */
+
+import chunk from '../chunk.js';
+
+describe('Chunk', () => {
+  it('should works', () => {
+    const result1 = chunk(['a', 'b', 'c', 'd'], 2);
+    expect(result1).toEqual([['a', 'b'], ['c', 'd']]);
+
+    const result2 = chunk(['a', 'b', 'c', 'd'], 3);
+    expect(result2).toEqual([['a', 'b', 'c'], ['d']]);
+
+    const result3 = chunk([], 4);
+    expect(result3).toEqual([]);
+
+    const result4 = chunk(['a'], 2);
+    expect(result4).toEqual([['a']]);
+
+    const result5 = chunk(['a', 'b', 'c', 'd', 'e', 'f'], 2);
+    expect(result5).toEqual([['a', 'b'], ['c', 'd'], ['e', 'f']]);
+  });
+});
