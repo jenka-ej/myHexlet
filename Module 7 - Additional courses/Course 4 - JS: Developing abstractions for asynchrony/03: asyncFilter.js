8335826ef41f49fd56562b45d53b8a64c4e1c07b
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2023-12-03 16:36:14 +0300

    Create 03: asyncFilter.js

---
+++ Module 7 - Additional courses/Course 4 - JS: Developing abstractions for asynchrony/03: asyncFilter.js
@@ -0,0 +1,76 @@
+// Реализуйте и экспортируйте по умолчанию функцию.
+
+// Примеры:
+// const coll = [10, 53, true, false, '', NaN, 22];
+// asyncFilter(coll, (v) => typeof v === 'number', (result) => {
+//   console.log(result); // => [10, 53, NaN, 22]
+// });
+
+// Так как фильтр асинхронный, то вызов предиката для каждого элемента коллекции должен выполняться после setTimeout().
+
+// Проверка на соответствие внутри фильтра должна быть нестрогой как и в обычном фильтре:
+// [10, 53, true, false, '', NaN, 22].filter(v => v);
+// // [ 10, 53, true, 22 ]
+
+export default function asyncFilter(coll, fn, callback) {
+  if (coll.length === 0) {
+    callback([]);
+    return;
+  }
+  const iter = (massive, acc) => {
+    const [head, ...rest] = massive;
+    const newAcc = fn(head) ? [...acc, head] : acc;
+    if (rest.length === 0) {
+      callback(newAcc);
+      return;
+    }
+    setTimeout(iter, 0, rest, newAcc);
+  };
+  iter(coll, []);
+}
+
+/* __tests__ */
+
+import asyncFilter from '../asyncFilter';
+
+test('should work', () => {
+  const coll = [];
+  return new Promise((done) => {
+    asyncFilter(coll, (x) => x, (result) => {
+      expect(result).toEqual(coll);
+      expect(result).not.toBe(coll);
+      done();
+    });
+  });
+});
+
+test('should work 2', () => {
+  const coll = [10, 53, true, false, '', NaN, 22];
+  return new Promise((done) => {
+    asyncFilter(coll, (x) => x, (result) => {
+      expect(result).toEqual([10, 53, true, 22]);
+      expect(result).not.toBe(coll);
+      done();
+    });
+  });
+});
+
+test('should work async', () => {
+  const coll = [1, 5, 2, 3, 4, 10, 9];
+  return new Promise((done) => {
+    asyncFilter(coll, (x) => x, (result) => {
+      expect(result).toEqual(coll);
+      expect(result).not.toBe(coll);
+      done();
+    });
+  });
+});
+
+test('function should be asynchronous', () => {
+  const coll = [1, 2, 3];
+  let actual = null;
+  asyncFilter(coll, (x) => x, (result) => {
+    actual = result;
+  });
+  expect(actual).toBeNull();
+});
